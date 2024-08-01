5c5b434ebe9b1eb348820c0ceda6418be5af1609
Gerardo Lanza <glanza007@gmail.com>
2024-08-01 08:37:38 -0600

    Creacion del proyecto

---
+++ Ambe.Frontend/Ambe.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ambe.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
